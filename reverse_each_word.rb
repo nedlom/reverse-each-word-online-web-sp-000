619de6b60535bcb1b04937c6277b5f92e32a7278
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-12 20:35:53 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+=begin
+def reverse_each_word(sentence)
+  x = sentence.split(" ")
+  y = []
+  x.each do |word|
+    y << word.reverse
+  end
+  y.join(" ")
+end
+=end
+
+def reverse_each_word(sentence)
+  x.sentence.split(" ")
+  x.map{|word|
